26341677a7e4f9596a1b02aca1f7f320cd446f82
alumno <alumno@A302PC15>
2021-10-13 11:51:02 +0200

    Agregar archivos de proyecto.

---
+++ Imagen/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Imagen
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AltaRadioButtonChecked(object sender, RoutedEventArgs e)
+        {
+            ImagenImage.Opacity = 1;
+        }
+
+        private void MediaRadioButtonChecked(object sender, RoutedEventArgs e)
+        {
+            ImagenImage.Opacity = 0.6;
+        }
+
+        private void BajaRadioButtonChecked(object sender, RoutedEventArgs e)
+        {
+            ImagenImage.Opacity = 0.3;
+        }
+
+        private void RellenoRadioButtonChecked(object sender, RoutedEventArgs e)
+        {
+            ImagenImage.Stretch = Stretch.Fill;
+        }
+
+        private void UniformeRadioButtonChecked(object sender, RoutedEventArgs e)
+        {
+            ImagenImage.Stretch = Stretch.Uniform;
+        }
+
+        private void RellenoUniformeRadioButtonChecked(object sender, RoutedEventArgs e)
+        {
+            ImagenImage.Stretch = Stretch.UniformToFill;
+        }
+
+        private void SinAjusteRadioButtonChecked(object sender, RoutedEventArgs e)
+        {
+            ImagenImage.Stretch = Stretch.None;
+        }
+    }
+}
